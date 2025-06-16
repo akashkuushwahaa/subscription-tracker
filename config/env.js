056b6214ec40fe8be1b44479336aa69e519c1fd6
akashkuushwahaa <akashkushwaha76986@gmail.com>
2025-06-16 15:45:34 +0530

    Setup and Config

---
+++ config/env.js
@@ -0,0 +1,7 @@
+import { config } from "dotenv";
+
+// Correct path formatting
+config({ path: `.env.${process.env.NODE_ENV || "development"}.local` });
+
+// Export environment variables
+export const { PORT, NODE_ENV } = process.env;
